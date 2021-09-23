e1a54c2cd139c41b67f7207ddec8b089bb329e05
Legfi <88987985+Legfi@users.noreply.github.com>
2021-09-24 00:33:37 +0200

    I am not a hope killer

--- text_reader.py
+++ text_reader.py
@@ -1,9 +1,7 @@
 from PIL import Image
 import streamlit as st
-import numpy as np
 import pandas as pd
 import requests
-
 def my_streamlit():
     """this function runs the app"""
     #subtitle
@@ -29,24 +27,49 @@ def my_streamlit():
     #chart = st.bar_chart(df)
     st.write(""" 
     ### Don't worry! Text Reader is here to help you during the hiring process of your company!""")
-    #getting user name and greeting
-    text = st.text_input("""Let's get start! Please Enter your name and press enter button below : """)
-    text_capitalize = text.capitalize()
-    if st.button("Enter :)"):
-        st.write('Welcom to Text Readers', text_capitalize, '!')
+    porpose = st.sidebar.selectbox('what is your porpose today?', ['working with a new cv', 'working with an old cv'])
+    if porpose == 'working with a new cv':
 
-    #here we get the users text and save them as a variable and press submit to use in our model
-    with st.form("this is a form"):
+        #getting user name and greeting
+        user_name = st.text_input("""Let's get start! Please Enter your condidate name and press enter button below : """)
+        user_name_c = user_name.capitalize()
+        if st.button("Enter :)"):
+            st.write('Allright! Let see if', user_name_c, 'is your favourit condidate!')
 
-        cv = st.text_input("""Allright! Just paste a copy of CV or Personal Letter you want to read 
-        and leave the rest to us! """)
-        user_question = st.text_input(""" Write an important question that you want to know about this CV: """)
-        finished = st.form_submit_button("Submit")
+        #here we get the users text and save them as a variable and press submit to use in our model
+        with st.form("this is a form"):
 
-        if finished:
-            model_response = my_module(cv, user_question)
-            st.write(model_response)
-    
+            cv = st.text_input("""OK! Just paste a copy of CV or Personal Letter you want to read 
+            and leave the rest to us! """)
+            user_question = st.text_input(""" Write an important question that you want to know about this CV: """)
+            finished = st.form_submit_button("Submit")
+
+            if finished:
+                model_response = my_module(cv, user_question)
+                st.write("Your answer is :", model_response["answer"])
+                st.write("Score of the right answer :", model_response["score"])
+                #class function here!!!you can write your codes to call functions in your class to save cvs into database
+                #here. delete these comments when you are done!!!
+                
+    elif porpose == 'working with an old cv':
+        st.text_input("""enter the condidates name that you want to work with their cv""")
+        #class function here!!!you can write your codes to call functions in your class to read cvs from database
+        #here. delete these comments when you are done!!!
+
+        ###############after your codes
+
+        #user_question = st.text_input(""" Write an important question that you want to know about this CV: """)
+        #finished = st.form_submit_button("Submit")
+        #here when user pick to work with old cv we need cv from your class to put in 
+        # my_module(class--> cv, user_question) which i dont know how. otherwise if it's hard we just decide
+        # that user only can see the cv or questions here and not interact with them which is what we need for
+        # our project that only show from data base!
+        #if finished:
+        #    model_response = my_module(user_question)
+        #    st.write(model_response)
+
+
+
 
 def my_module(cv, user_question):
     #this function gets cv and user_question and send them to our module
@@ -58,6 +81,7 @@ def my_module(cv, user_question):
     response = requests.post(url, json=body)
     return response.json()
 
+
 def main():
 
     my_streamlit()
@@ -68,11 +92,3 @@ def main():
 if __name__ == "__main__":
     
     main()
-    
-
-    
-
-
-    #filtering our data(i mon av tid annars tar vi bort dem!)
-    #st.sidebar.slider('How many years of experience?', 0, 10, 5)
-    #st.sidebar.multiselect('Which field?', ['IT', 'Engineering', 'Art'])
