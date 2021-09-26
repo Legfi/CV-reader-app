d2ca027a73d2ccaee6f532024f704c313af08f69
K-F.Hagman <fredde.hagman@gmail.com>
2021-09-26 19:01:35 +0200

    is this cleaner code?

--- text_reader.py
+++ text_reader.py
@@ -46,17 +46,15 @@ def delete_candidate(name):
 
 def my_streamlit():
     """this function runs the app"""
-# Landing page -------------------------------------------------------------------------------------------------------------
+# -------------------------------------------------Landing page-------------------------------------------------------------
 # Tile/ application pitch
-    st.write("""
-    # We make your job as a manager much easier than before!""")
-    st.write("""
-    ## Are you tired of reading thousands of CV's every day and still not find a person you need for your company?""")
+    st.write("""# We make your job as a manager much easier than before!""")
+    st.write("""## Are you tired of reading thousands of CV's every day and still not find a person you need for your company?""")
 
 # Background image(quality is very bad we should change the image later!)
     image = Image.open("I'm tired CV.jpg")
     st.image(image, caption="I'm done with this!", use_column_width=True)
-# Landing page -------------------------------------------------------------------------------------------------------------
+# -------------------------------------------------Landing page-------------------------------------------------------------
 
 
 # ------------------------------------Statistics of how much time you can save----------------------------------------------
@@ -74,24 +72,23 @@ def my_streamlit():
 
 
 #  ----------------------------------------Multiple choice selectionbox-----------------------------------------------------
-    st.write(""" ### Don't worry! Text Reader is here to help you during the hiring process of your company!
-    ###
-    ###
-    ### """)
-    purpose = st.selectbox('Would you like to add new candidate, or review existing candidates in database', ['Add new candidate', 'Review existing candidates'])
+    st.write("""### Don't worry! Text Reader is here to help you during the hiring process of your company!""")
+    purpose = st.selectbox(
+        'Would you like to add new candidate, or review existing candidates in database', 
+        ['Add new candidate', 'Review existing candidates'])
 #  ----------------------------------------Multiple choice selectionbox-----------------------------------------------------
 
 
 # ---------------------------------Streamlit form for adding new candidate cv to database-----------------------------------
     if purpose == 'Add new candidate':
-        st.write(""" 
-        ###### Enter name and paste cv or personal letter and press the upload button to add new candidate""")
+        st.write("""Enter name and paste cv or personal letter and press the upload button to add new candidate""")
 
         with st.form("Upload candidate form"):
-            candidate_name = st.text_input("""Enter candidate name : """).capitalize()
-            candidate_cv = st.text_input("""Paste a copy of CV or Personal Letter""")
-            upload = st.form_submit_button("Upload")
+            candidate_name  = st.text_input("""Enter candidate name : """).capitalize()
+            candidate_cv    = st.text_input("""Paste a copy of CV or Personal Letter""")
 
+# Submitbutton
+            upload          = st.form_submit_button("Upload")
             if upload:
 # Database Create function
                 add_candidate(candidate_name, candidate_cv)
@@ -100,36 +97,39 @@ def my_streamlit():
 
 # ----------------------------------Streamlit form for submitting question to ML-model--------------------------------------
     elif purpose == 'Review existing candidates':
-        st.write(""" 
-        ###### First select a candidate in the dropdown menu, then type in a question and press submit to get your answer""")
+        st.write("""First select a candidate in the dropdown menu, then type in a question and press submit to get your answer""")
 
 # creating list of candidate names names from database candidates.db
         cursor.execute("SELECT name FROM candidates_cv")
         names = cursor.fetchall()
-        list_of_candidates = []
+        list_of_candidates  = []
         for row in names:
             list_of_candidates.append(row[0])
+
 # question form and candidate selection box
         with st.form("Question form"):
-            selected_candidate = st.selectbox('Candidates', list_of_candidates)
-            candidate_cv = str(read_candidate_cv(selected_candidate))
-            user_question = st.text_input(""" Write an important question that you want to know about this candidates CV: """)
-            submit_question = st.form_submit_button("Submit")
+            selected_candidate  = st.selectbox('Candidates', list_of_candidates)
+            candidate_cv        = str(read_candidate_cv(selected_candidate))
+            user_question       = st.text_input(""" Write an important question that you want to know about this candidates CV: """)
+            submit_question     = st.form_submit_button("Submit")
             
             if submit_question:
-                model_response = my_module(candidate_cv, user_question)
-                st.write(model_response)
+                model_response  = my_module(candidate_cv, user_question)
+                # st.write(model_response)
+                st.write("Your answer is :", model_response["answer"])
+                st.write("Score of the right answer :", model_response["score"])
+# ----------------------------------Streamlit form for submitting question to ML-model--------------------------------------
 
 
 
 def my_module(cv, user_question):
     #this function gets cv and user_question and send them to our module
     
-    r = requests.post('http://localhost:8000/start', json={'name' : 'question_answering'})
+    requests.post('http://localhost:8000/start', json = {'name' : 'question_answering'})
     
-    url = "http://localhost:8000/qa"
-    body = {"context": cv , "question":user_question}
-    response = requests.post(url, json=body)
+    url         = "http://localhost:8000/qa"
+    body        = {"context": cv , "question":user_question}
+    response    = requests.post(url, json=body)
     return response.json()
 
 
